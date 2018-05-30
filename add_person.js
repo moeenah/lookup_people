30f9610a927bfac03fd45abfe7937411298226d1
moeenah <moeen.ahmad97@hotmail.com>
2018-05-30 00:07:14 +0000

    server auto closes for add_person and lookup_people_knex

--- add_person.js
+++ add_person.js
@@ -11,14 +11,23 @@ var knex = require('knex')({
   }
 });
 
-knex('famous_people')
-.insert([
-  {first_name: process.argv[2],
-   last_name: process.argv[3],
-    birthdate: process.argv[4]}
-    ])
-.asCallback(function(error, rows) {
-    knex.select('*').from('famous_people').asCallback(function(error, rows) {
-    console.log(rows);
+if (process.argv[2] === undefined || process.argv[3] === undefined || process.argv[4] === undefined) {
+  console.log('Please enter a first name AND a last name AND a birthdate');
+} else {
+  knex('famous_people')
+  .insert([
+    {first_name: process.argv[2],
+     last_name: process.argv[3],
+      birthdate: process.argv[4]}
+      ])
+  .asCallback(function(error, rows) {
+      knex.select('*').from('famous_people').asCallback(function(error, rows) {
+      console.log(rows);
+       //on search completion, exits program
+      process.on("exit", function() {
+        console.log("\nExiting..");
+      });
+      process.exit();
+    });
   });
-});
+}
